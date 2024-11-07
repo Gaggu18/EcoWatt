78e85035e2a75878b64838a06e6e13002cd8c118
Gagandeep Singh <terenaalishqa@gmail.com>
2024-11-08 02:46:58 +0530

    more change

--- ecowatt/urls.py
+++ ecowatt/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('energy/', include('energy.urls')),
+    path('/', include('energy.urls')),
 ]
